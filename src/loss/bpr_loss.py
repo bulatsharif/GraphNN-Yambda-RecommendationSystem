8d981085d40eb6504d09fe383423f633ef145e3c
Danil Fathutdinov <fatkhutdinov2005@bk.ru>
2025-11-22 22:48:25 +0300

    feat: Add bpr loss implementation

---
+++ src/loss/bpr_loss.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+
+class BPRLoss(nn.Module):
+    def __init__(self, reduction='mean'):
+        super(BPRLoss, self).__init__()
+        self.reduction = reduction
+        self.log_sigmoid = nn.LogSigmoid()
+
+    def forward(self, positive_scores, negative_scores):
+        diff = positive_scores - negative_scores
+
+        loss = -self.log_sigmoid(diff)
+        
+        if self.reduction == 'mean':
+            return loss.mean()
+        elif self.reduction == 'sum':
+            return loss.sum()
+        else:
+            return loss
